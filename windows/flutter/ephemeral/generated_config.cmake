6026dfdfc801176f73a12d393f5d6cdc33c62ea3
Vandan Arora <aroravandan337@gmail.com>
2026-08-30 20:04:03 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Carrier lock\\carrierlock" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=D:\\Carrier lock\\carrierlock"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Carrier lock\\carrierlock\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Carrier lock\\carrierlock"
+  "FLUTTER_TARGET=D:\\Carrier lock\\carrierlock\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OTI0MTM0YTQ0Yw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzQxNmFjZmViOA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Carrier lock\\carrierlock\\.dart_tool\\package_config.json"
+)
